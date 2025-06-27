2838772bfdd27e248672e06c87417b1f95295c99
JUNICHI WAKABAYASHI <jun1@rimgate.net>
2025-06-27 14:59:35 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,3 +4,33 @@ const switches = document.querySelectorAll('.switch');
 const calculations = document.querySelectorAll('.calculation');
 const resets = document.querySelectorAll('.reset');
 const results = document.querySelectorAll('.result');
+
+// --- 2. イベントリスナーの設定 ---
+// forEachを使って、取得したボタンのリストそれぞれにクリックイベントを設定していく
+
+// 数字ボタンの処理
+switches.forEach(function (button) {
+    button.addEventListener('click', function (e) {
+        console.log('数字ボタン:', e.target.textContent);
+    });
+});
+// 演算子ボタンの処理
+calculations.forEach(function (button) {
+    button.addEventListener('click', function (e) {
+        console.log('演算子ボタン:', e.target.textContent);
+    });
+});
+
+// ACボタンの処理
+resets.forEach(function (button) {
+    button.addEventListener('click', function () {
+        console.log('ACボタンが押されました');
+    });
+});
+
+// =ボタンの処理
+results.forEach(function (button) {
+    button.addEventListener('click', function () {
+        console.log('=ボタンが押されました');
+    });
+});
