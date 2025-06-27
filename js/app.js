6323a5dbce4534ca827face25ec09a9c7ae97dc6
JUNICHI WAKABAYASHI <jun1@rimgate.net>
2025-06-27 14:48:35 +0900

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// --- 1. HTML要素の取得 ---
+// まずは操作したいHTML要素をすべて取得して、変数に入れておく
+const switches = document.querySelectorAll('.switch');
+const calculations = document.querySelectorAll('.calculation');
+const resets = document.querySelectorAll('.reset');
+const results = document.querySelectorAll('.result');
